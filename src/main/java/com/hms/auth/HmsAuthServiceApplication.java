1c8f6cb555ef4121209889fd3e9a3485a7e6e2ef
dineshbudime <dineshbudime123@gmail.com>
2025-12-23 12:21:00 +0530

    auth-template for hms-auth-service

---
+++ src/main/java/com/hms/auth/HmsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hms.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsAuthServiceApplication.class, args);
+	}
+
+}
